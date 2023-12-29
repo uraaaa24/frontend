20e27a407fd6087d70218f2d131aff903f62db4b
uraaaa24 <yuto.urashima24@gmail.com>
2023-12-29 14:30:03 +0000

    apiの例を追加

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,3 +1,14 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  const [data, setData] = useState({name: "初期値"})
+
+  useEffect(() => {
+    const change = {name: "変更"}
+    setData(change)
+  }, [])
+
+  return <h1>Hello, {data.name}</h1>
 }
