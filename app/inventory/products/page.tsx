1b8481e0e09aadd822f2fbe89cdf980c7e229699
Yuto Urashima <yuto.urashima24@gmail.com>
2023-12-31 13:51:46 +0000

    動的描画の導入

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -1,4 +1,22 @@
+'use client'
+
+import productsData from '@/dummyData/products.json'
+import { useEffect, useState } from 'react'
+
+type ProductData = {
+    id: number
+    name: string
+    price: number
+    description: string
+}
+
 export default function Page() {
+    const [data, setData] = useState<Array<ProductData>>([])
+
+    useEffect(() => {
+        setData(productsData)
+    }, [])
+
     return (
         <>
             <h2>商品一覧</h2>
@@ -14,40 +32,17 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>コットン100%バッグリボンティアードワンピース（黒）</td>
-                        <td>6900</td>
-                        <td>
-                            大人の愛らしさを引き立てる、ナチュラルな風合い。リラックス ×
-                            トレンドを楽しめる、上品なティアードワンピース。
-                        </td>
-                        <td>
-                            <button>更新・削除</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td> ライトストレッチカットソー（ネイビー）</td>
-                        <td>2980</td>
-                        <td>
-                            しなやかな肌触りが心地よい、程よいフィット感のカットソー。ビジネスカジュアルにも普段使いにも使える、ベーシックなデザイン。
-                        </td>
-                        <td>
-                            <button>更新・削除</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>ベルト付きデニムパンツ（ブルー）</td>
-                        <td>5980</td>
-                        <td>
-                            定番のデニムパンツに、フェルみんなベルトをプラスしたスタイリッシュなアイテム。カジュアルにもキレイめにも合わせやすい。
-                        </td>
-                        <td>
-                            <button>更新・削除</button>
-                        </td>
-                    </tr>
+                    {data.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.name}</td>
+                            <td>{data.price}</td>
+                            <td>{data.description}</td>
+                            <td>
+                                <button>更新・削除</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </>
